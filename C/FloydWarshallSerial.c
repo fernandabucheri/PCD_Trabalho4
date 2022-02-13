1855f6db7baeec34e5cc13516e7b41f8e1aad88e
Marcos Villar <marcosvillar96@gmail.com>
2022-02-13 19:54:20 -0300

    refactor: minor code refactor

--- C/FloydWarshallSerial.c
+++ C/FloydWarshallSerial.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <time.h>
 
 #define V 5000
 
-void floydWarshall(int **graph)
-{
-	int **dist;
-	int i, j, k;
+void floydWarshall(int **graph) {
+    int **dist;
+    int i, j, k;
 
-	dist = malloc(V * sizeof(int *));
-	for (i = 0; i < V; i++)
-		dist[i] = malloc(V * sizeof(int));
+    dist = malloc(V * sizeof(int *));
+    for (i = 0; i < V; i++)
+        dist[i] = malloc(V * sizeof(int));
 
-	for (i = 0; i < V; i++)
-		for (j = 0; j < V; j++)
-			dist[i][j] = graph[i][j];
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
 
-	for (k = 0; k < V; k++)
-		for (i = 0; i < V; i++)
-			for (j = 0; j < V; j++)
-				if (dist[i][k] + dist[k][j] < dist[i][j])
-					dist[i][j] = dist[i][k] + dist[k][j];
+    for (k = 0; k < V; k++)
+        for (i = 0; i < V; i++)
+            for (j = 0; j < V; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
 }
 
-int main()
-{
+int main() {
 
-	int **graph;
-	int i, j;
-	time_t start, end;
+    int **graph;
+    int i, j;
+    time_t start, end;
 
-	graph = malloc(V * sizeof(int *));
+    graph = malloc(V * sizeof(int *));
 
-	for (i = 0; i < V; i++)
-		graph[i] = malloc(V * sizeof(int));
+    for (i = 0; i < V; i++)
+        graph[i] = malloc(V * sizeof(int));
 
-	for (i = 0; i < V; i++)
-		for (j = 0; j < V; j++)
-			graph[i][j] = rand() % 2500;
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            graph[i][j] = rand() % 2500;
 
-	time(&start);
-	floydWarshall(graph);
-	time(&end);
-	printf("\nTempo de execução : %ld segundos.\n", end - start);
-	return 0;
+    time(&start);
+    floydWarshall(graph);
+    time(&end);
+    printf("\nTempo de execução : %ld segundos.\n", end - start);
+    return 0;
 }
--- C/FloydWarshallThreads.c
+++ C/FloydWarshallThreads.c
@@ -1,55 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <limits.h>
 #include <time.h>
 
 #define V 5000
 
-void floydWarshall(int **graph, int N_THREADS)
-{
-	int **dist;
-	int i, j, k;
+void floydWarshall(int **graph, int N_THREADS) {
+    int **dist;
+    int i, j, k;
 
-	dist = malloc(V * sizeof(int *));
-	for (i = 0; i < V; i++)
-		dist[i] = malloc(V * sizeof(int));
+    dist = malloc(V * sizeof(int *));
+    for (i = 0; i < V; i++)
+        dist[i] = malloc(V * sizeof(int));
 
 #pragma omp parallel for shared(dist, graph) private(i, j) num_threads(N_THREADS)
-	for (i = 0; i < V; i++)
-		for (j = 0; j < V; j++)
-			dist[i][j] = graph[i][j];
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
 
 #pragma omp parallel for shared(dist) private(i, j, k) num_threads(N_THREADS)
-	for (k = 0; k < V; k++)
-		for (i = 0; i < V; i++)
-			for (j = 0; j < V; j++)
-				if (dist[i][k] + dist[k][j] < dist[i][j])
-					dist[i][j] = dist[i][k] + dist[k][j];
+    for (k = 0; k < V; k++)
+        for (i = 0; i < V; i++)
+            for (j = 0; j < V; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
 }
 
-int main()
-{
+int main() {
 
-	int **graph;
-	int i, j, N_THREADS;
-	time_t start, end;
+    int **graph;
+    int i, j, N_THREADS;
+    time_t start, end;
 
-	graph = malloc(V * sizeof(int *));
+    graph = malloc(V * sizeof(int *));
 
-	for (i = 0; i < V; i++)
-		graph[i] = malloc(V * sizeof(int));
+    for (i = 0; i < V; i++)
+        graph[i] = malloc(V * sizeof(int));
 
-	printf("Digite o número de threads: \n");
-	scanf("%d", &N_THREADS);
+    printf("Digite o número de threads: \n");
+    scanf("%d", &N_THREADS);
 
-	for (i = 0; i < V; i++)
-		for (j = 0; j < V; j++)
-			graph[i][j] = rand() % 2500;
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            graph[i][j] = rand() % 2500;
 
-	time(&start);
-	floydWarshall(graph, N_THREADS);
-	time(&end);
-	printf("\nTempo de execução : %ld segundos.\n", end - start);
-	return 0;
+    time(&start);
+    floydWarshall(graph, N_THREADS);
+    time(&end);
+    printf("\nTempo de execução : %ld segundos.\n", end - start);
+    return 0;
 }
